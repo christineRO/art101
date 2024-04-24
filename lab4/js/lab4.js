d377f144d392e0639ef4b566874d086724dd27ff
christineRO <44623867+christineRO@users.noreply.github.com>
2024-04-23 21:39:51 -0700

    lab4-a
    fix:
    - lab3 task 6/7 (sc)
    - lab4 task 3 (sc)
    - lab4 report link

---
+++ lab4/js/lab4.js
@@ -0,0 +1,17 @@
+//GAME
+
+//Rock Paper Scissors
+
+//Have basic UI, AKA homepage, where:
+    //There is an input/are button(s)
+    //Some cool CSS
+//Take input from user
+//Randomize computer's chance of rock, paper, or scissors
+//Compare the two inputs of human and machine
+    //(Unless I find a way other than booleans, then this is the crap you're getting:)
+    //Rock > scissors
+    //Scissors > paper
+    //Paper > rock
+//Display the result of the game
+
+//boom
