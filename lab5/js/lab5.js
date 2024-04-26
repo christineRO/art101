442d5663494657c3a50f0d89c1d9a8541c5b3459
christineRO <44623867+christineRO@users.noreply.github.com>
2024-04-25 20:15:32 -0700

    lab5
    fix:
    - lab3 task 6/7 (sc)
    - lab4 task 3 (sc)
    - lab4 report link
    - lab5 report link

---
+++ lab5/js/lab5.js
@@ -0,0 +1,23 @@
+/*
+
+    Author: Christine
+    Created: 04.25.24
+
+    */
+
+// variables:
+var make = "Hyundai";
+var model = "Elantra";
+var color = "Silver";
+var year = 2013;
+var ownIt = true;
+var age = 2024 - year;
+
+// output:
+    document.writeln("Make: " + make + "<br>");
+    document.writeln("Model: " + model + "<br>");
+    document.writeln("Color: " + color + "<br>");
+    document.writeln("Year: " + year + "<br>");
+    document.writeln("Ownership: " + ownIt + "<br>");
+    document.writeln("Age: " + age + " years<br>"); 
+
