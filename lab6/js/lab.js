8d536d3f573c17b8141e9fc40a6385732a0406c2
christineRO <44623867+christineRO@users.noreply.github.com>
2024-04-28 22:42:51 -0700

    lab6
    fix:
    - lab3 task 6/7 (sc)
    - lab4 task 3 (sc)
    - lab4 report link
    - lab5 report link
    - lab6 report link
    
    did:
    - lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,23 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Christine Role
+// Date: 28, April 2024
 
-// Constants
+// Task 2: A JavaScript Program
 
-// Functions
+// Defining the variables:
+var myTransport = ['car', 'bus', 'legs'];
+var myMainRide = {
+  make: "Hyundai",
+  model: "Elantra",
+  color: "Silver",
+  year: 2013,
+  age: function() {
+    return 2024 - this.year;
+  }
+};
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Using document.writeln() to output myTransport:
+document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
